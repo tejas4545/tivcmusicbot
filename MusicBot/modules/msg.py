91f2555062810c3f98e7203a4fd202b432b19bb1
tejas4545 <65526665+tejas4545@users.noreply.github.com>
2021-06-18 19:38:55 +0530

    Update msg.py

--- MusicBot/modules/msg.py
+++ MusicBot/modules/msg.py
@@ -1,7 +1,7 @@
 import os
 from MusicBot.config import SOURCE_CODE,ASSISTANT_NAME,PROJECT_NAME,SUPPORT_GROUP,UPDATES_CHANNEL
 class Messages():
-      START_MSG = "**Hello 👋 [{}](tg://user?id={})!**\n\n🤖 I am an advanced bot created for playing music in the voice chats of Telegram Groups & Channels.\n\n✅ Send me /help for more info."
+      START_MSG = "**Hello 👋 [{}](tg://user?id={})!**\n\n🤖 I am an advanced bot created By Tejas for playing music in the voice chats of Telegram Groups & Channels.\n\n✅ Send me /help for more info."
       HELP_MSG = [
         ".",
 f"""
